afb38339a15796ac3a169ae53547b164853d3b03
heyhizlan <m.hizlan@icloud.com>
2026-08-17 16:56:37 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || '/',
-  plugins: [react(), tailwindcss()],
-})
-
-export default defineConfig({
   build: {
     rollupOptions: {
       output: {
