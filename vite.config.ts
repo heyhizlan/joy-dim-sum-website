fd76b812a95c242c864006c3314b83a4f3ae3ee4
heyhizlan <m.hizlan@icloud.com>
2026-08-17 16:55:27 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,3 +6,13 @@ export default defineConfig({
   base: process.env.VITE_BASE_PATH || '/',
   plugins: [react(), tailwindcss()],
 })
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+      },
+    },
+  },
+});
